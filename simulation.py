19177a59a5965ecc613a055092381dc04be4aa43
Konstantina Dachlythra <koda4949@sol-login.fysik.su.se>
2023-06-07 16:45:20 +0200

    mostly adding parser arguments

--- simulation.py
+++ simulation.py
@@ -2,74 +2,117 @@ import utils as ut
 import healpy as hp
 import numpy as np
 import os
-from optparse import OptionParser
+import argparse as ap
+import os 
+import sys
 
-parser = OptionParser()
-parser.add_option('--output-dir', dest='dirname', default='none',
+opj = os.path.join
+
+parser = ap.ArgumentParser(formatter_class=\
+ap.ArgumentDefaultsHelpFormatter)
+
+parser.add_argument('--data-path', dest='data_path', default='none',
+                  type=str, help='Directory containing the simulation data')
+parser.add_argument('--initial-param-file', dest='initial_param_file', default='none',
+                  type=str, help='Directory containing the initial fitting params')
+parser.add_argument('--band-names', dest='band_names', action='store', default=None,
+                  type=str, nargs='+', help='Frequency band names')
+parser.add_argument('--parse-bms-fwhm', dest='parse_bms_fwhm', action='store', default=[True,True,True,True,True,True],
+                  help='Simply parse the beam best-fit FWHM to smooth the maps')
+parser.add_argument('--epsilon-fwhms', dest='epsilon_fwhms', action='store', default=[0.,0.,0.,0.,0.,0.],
+                  nargs='+', type=float, help='Perturb the default beam sizes by some amount in arcminutes')
+parser.add_argument('--do-freq-scaling', dest='do_freq_scaling', action='store', default=[False,False,False,False,False,False],
+                  nargs='+', help='Frequency scale beams of which frequency bands')
+parser.add_argument('--beam-type', dest='beam_type', action='store',
+                  default = ['Gaussian','Gaussian','Gaussian','Gaussian','Gaussian','Gaussian'],
+                  help='Work with Gaussian or PO beams.')
+parser.add_argument('--save-params', dest='save_params', action='store',
+                  help='Boolean, save simulation parameters.')
+parser.add_argument('--simple-sky', dest='simple_sky', default=False, action='store_true',
+                  help='Spectral index of dust and synchrotron are fixed.')
+parser.add_argument('--beam-path', dest='beam_path', action='store',
+                  default='/cfs/home/koda4949/simonsobs/beam_chromaticity/input_beams',
+                  help='Provide beam path, if available, of full beam healpix maps.')
+parser.add_argument('--conv_space', dest='conv_space', action='store',
+                  default='harmonic', help='Beam convolution happens on map or harmonic domain.')
+parser.add_argument('--conv-sed-type', dest='conv_sed_type', action='store',
+                  default='plaw', help='Scale by a simple power low if True else use compoenents SED.\
+                  Choose between plaw or comp_sed.')
+parser.add_argument('--conv-nu-c', dest='conv_nu_c', action='store',
+                  default='band_centers', help='The reference frequencies to be used for scaing.\
+                  Choose from band_centers, bpass_centers or fg_centers.')
+parser.add_argument('--output-dir', dest='dirname', default='none',
                   type=str, help='Output directory')
-parser.add_option('--seed', dest='seed',  default=1000, type=int,
+parser.add_argument('--prefix', dest='prefix', default='none',
+                  type=str, help='Prefix for storing file')
+parser.add_argument('--seed', dest='seed',  default=1000, type=int,
                   help='Set to define seed, default=1000')
-parser.add_option('--nside', dest='nside', default=256, type=int,
+parser.add_argument('--lmax', dest='lmax',  default=None, type=int,
+                  help='Set to maximum multipole number. If None lmax=3*nside-1.')
+parser.add_argument('--nside', dest='nside', default=256, type=int,
                   help='Set to define Nside parameter, default=256')
-parser.add_option('--std-dust', dest='std_dust', default=0., type=float,
+parser.add_argument('--std-dust', dest='std_dust', default=0., type=float,
                   help='Deviation from the mean value of beta dust, default = 0.')
-parser.add_option('--std-sync', dest='std_sync', default=0., type=float,
+parser.add_argument('--std-sync', dest='std_sync', default=0., type=float,
                   help='Deviation from the mean value of beta synchrotron, default = 0.')
-parser.add_option('--gamma-dust', dest='gamma_dust', default=-3., type=float,
+parser.add_argument('--gamma-dust', dest='gamma_dust', default=-3., type=float,
                   help='Exponent of the beta dust power law, default=-3.')
-parser.add_option('--gamma-sync', dest='gamma_sync', default=-3., type=float,
+parser.add_argument('--gamma-sync', dest='gamma_sync', default=-3., type=float,
                   help='Exponent of the beta sync power law, default=-3.')
-parser.add_option('--include-cmb', dest='include_cmb', default=True, action='store_false',
+parser.add_argument('--include-cmb', dest='include_cmb', default=True, action='store_false',
                   help='Set to remove CMB from simulation, default=True.')
-parser.add_option('--include-sync', dest='include_sync', default=True, action='store_false',
+parser.add_argument('--include-sync', dest='include_sync', default=True, action='store_false',
                   help='Set to remove synchrotron from simulation, default=True.')
-parser.add_option('--include-dust', dest='include_dust', default=True, action='store_false',
+parser.add_argument('--include-dust', dest='include_dust', default=True, action='store_false',
                   help='Set to remove dust from simulation, default=True.')
-parser.add_option('--include-E', dest='include_E', default=True, action='store_false',
+parser.add_argument('--include-E', dest='include_E', default=True, action='store_false',
                   help='Set to remove E-modes from simulation, default=True.')
-parser.add_option('--include-B', dest='include_B', default=True, action='store_false',
+parser.add_argument('--include-B', dest='include_B', default=True, action='store_false',
                   help='Set to remove B-modes from simulation, default=True.')
-parser.add_option('--mask', dest='add_mask', default=False, action='store_true',
+parser.add_argument('--mask', dest='add_mask', default=False, action='store_true',
                   help='Set to add mask to observational splits, default=False.')
-parser.add_option('--dust-vansyngel', dest='dust_vansyngel', default=False, action='store_true',
+parser.add_argument('--dust-vansyngel', dest='dust_vansyngel', default=False, action='store_true',
                   help='Set to use Vansyngel et al\'s dust model, default=False.')
-parser.add_option('--beta', dest='gaussian_beta', default=True, action='store_false',
+parser.add_argument('--beta', dest='gaussian_beta', default=True, action='store_false',
                   help='Set for non-gaussian beta variation.')
-parser.add_option('--nu0-dust', dest='nu0_dust', default=353., type=int,
+parser.add_argument('--nu0-dust', dest='nu0_dust', default=353., type=int,
                   help='Set to change dust pivot frequency, default=353 GHz.')
-parser.add_option('--nu0-sync', dest='nu0_sync', default=23., type=int,
+parser.add_argument('--nu0-sync', dest='nu0_sync', default=23., type=int,
                   help='Set to change synchrotron pivot frequency, default=23 GHz.')
-parser.add_option('--A-dust-BB', dest='Ad', default=5, type=float,
+parser.add_argument('--A-dust-BB', dest='Ad', default=5, type=float,
                   help='Set to modify the B-mode dust power spectrum amplitude, default=5')
-parser.add_option('--alpha-dust-BB', dest='alpha_d', default=-0.42, type=float,
+parser.add_argument('--alpha-dust-BB', dest='alpha_d', default=-0.42, type=float,
                   help='Set to mofify tilt in D_l^BB for dust, default=-0.42')
-parser.add_option('--A-sync-BB', dest='As', default=2, type=float,
+parser.add_argument('--A-sync-BB', dest='As', default=2, type=float,
                   help='Set to modify the B-mode dust power spectrum amplitude, default=2')
-parser.add_option('--alpha-sync-BB', dest='alpha_s', default=-0.6, type=float,
+parser.add_argument('--alpha-sync-BB', dest='alpha_s', default=-0.6, type=float,
                   help='Set to mofify tilt in D_l^BB for synchrotron, default=-0.42')
-parser.add_option('--plaw-amp', dest='plaw_amps', default=True, action='store_false',
+parser.add_argument('--plaw-amp', dest='plaw_amps', default=True, action='store_false',
                   help='Set to use realistic amplitude maps for dust and synchrotron.')
-parser.add_option('--r-tensor', dest='r_tensor', default=0.0, type=float,
+parser.add_argument('--r-tensor', dest='r_tensor', default=0.0, type=float,
                   help='Set to mofify tensor-to-scalar ratio')
-parser.add_option('--dust-sed', dest='dust_sed', default='mbb', type=str,
+parser.add_argument('--dust-sed', dest='dust_sed', default='mbb', type=str,
                   help='Dust SED (\'mbb\' or \'hensley_draine\' or \'curved_plaw\')')
-parser.add_option('--sync-sed', dest='sync_sed', default='plaw', type=str,
+parser.add_argument('--sync-sed', dest='sync_sed', default='plaw', type=str,
                   help='Synchrotron SED (\'plaw\' or \'curved_plaw\')') #TODO: curved to add
 ## NEW
-parser.add_option('--dust-beta', dest='dust_beta', default='none', type=str,
+parser.add_argument('--dust-beta', dest='dust_beta', default='none', type=str,
                   help='Non-plaw dust beta map: leave none for d1, use GNILC for d10.')
-parser.add_option('--dust-amp', dest='dust_amp', default='none', type=str,
+parser.add_argument('--dust-amp', dest='dust_amp', default='none', type=str,
                   help='Non-plaw dust amplitude map: leave none for d1, use GNILC for d10.')
-parser.add_option('--unit-beams', dest='unit_beams', default=False, action='store_true',
+parser.add_argument('--unit-beams', dest='unit_beams', default=False, action='store_true',
                   help='Set to include unitary beams instead of SO-like beams, default=False.')
 
-(o, args) = parser.parse_args()
-
+o = parser.parse_args()
 nside = o.nside
 seed = o.seed
 
+print('epsilon fwhms=',o.epsilon_fwhms)
 if o.dirname == 'none':
     o.dirname = "./" 
+
+if not os.path.exists(o.dirname):
+  os.makedirs(o.dirname)
 
 o.dirname += "sim_ns%d" %o.nside
 
@@ -123,9 +166,15 @@ else:
     o.dirname+= "_SObeams"
 
 o.dirname+="/s%d" % o.seed
-
 os.system('mkdir -p ' + o.dirname)
 print(o.dirname)
+
+if o.save_params:
+  arg_dict = {}
+  for arg in vars(o):
+    arg_dict[arg] = getattr(o, arg)
+  np.save(opj(o.dirname, 'simulation_parameters.txt'), arg_dict)
+
 
 # Decide whether spectral index is constant or varying
 mean_p, moment_p = ut.get_default_params()
@@ -175,24 +224,50 @@ mean_p['dust_beta_map'] = o.dust_beta
 # Define if we're using unit or SO-like beams
 mean_p['unit_beams'] = o.unit_beams
 
+lmax = o.lmax
+if lmax is None:
+   lmax = 3*o.nside-1
 
 ### Theory prediction, simulation and noise
-scc = ut.get_theory_sacc(o.nside, mean_pars=mean_p,
+scc = ut.get_theory_sacc(o.data_path, o.band_names, o.nside, lmax=lmax, mean_pars=mean_p,
                          moment_pars=moment_p, add_11=True, add_02=False)
 ##scc.saveToHDF(o.dirname+"/cells_model.sacc") #old sacc
 scc.save_fits(o.dirname+'/cls_fid.fits', overwrite=True)
 
 if o.dust_vansyngel:
     mean_p['include_dust'] = False
-sim = ut.get_sky_realization(o.nside, seed=o.seed,
+
+sim = ut.get_sky_realization(o.data_path, o.band_names, o.nside, seed=o.seed,
                              mean_pars=mean_p,
                              moment_pars=moment_p,
                              gaussian_betas=o.gaussian_beta,
                              plaw_amps=o.plaw_amps,
-                             compute_cls=True)
+                             compute_cls=True,
+                             lmax=lmax,
+                             simple_sky=o.simple_sky,
+                             **{'epsilon_fwhms':o.epsilon_fwhms,
+                                'parse_bms_fwhm':np.array(o.parse_bms_fwhm),
+                                'space':o.conv_space,
+                                'do_scaling':np.array(o.do_freq_scaling),
+                                'beam_type':o.beam_type,
+                                'beam_path':o.beam_path,
+                                'sed_type':o.conv_sed_type,
+                                'nu_c':o.conv_nu_c,
+                                'outdir': '../input_beams/test_fscaled_beams',
+                                'prefix':o.prefix,
+                                'tele':'SAT',
+                                'lon_rot':0,
+                                'lat_rot':90,
+                                'lon_crop':[-3,3],
+                                'lat_crop':[-3,3],
+                                'initial_param_file':o.initial_param_file,
+                                'n_iter':100,
+                                'acc':0.1,
+                                'res':0.001})
+
 if o.dust_vansyngel:
     import utils_vansyngel as uv
-    nus = ut.get_freqs()
+    nus = ut.get_freqs(o.band_names)
     qud = np.transpose(np.array(uv.get_dust_sim(nus, o.nside)),
                        axes=[1, 0, 2])
     units = ut.fcmb(nus)
@@ -209,19 +284,28 @@ if o.dust_vansyngel:
             qud[inu, :, :] = ut.eb2qu(ebd, o.nside, lmax)
     sim['freq_maps'] += qud
 
-noi = ut.create_noise_splits(o.nside)
+noi = ut.create_noise_splits(o.data_path, o.band_names, o.nside, lmax=lmax)
 
 # Define maps signal and noise
 mps_signal = sim['freq_maps']
 mps_noise = noi['maps_noise']
 
-# Save sky maps
-nu = ut.get_freqs()
+#print('np.shape(mps_noise)=',np.shape(mps_noise))
+#for s in range(4):
+#   hp.write_map(o.dirname+"/maps_noise_"+str(s)+".fits", mps_noise[s],
+#             overwrite=True)
+
+# Save beam maps
+nu = ut.get_freqs(o.band_names)
 nfreq = len(nu)
 npol = 2
 nmaps = nfreq*npol
 npix = hp.nside2npix(o.nside)
-hp.write_map(o.dirname+"/maps_sky_signal.fits", mps_signal.reshape([nmaps,npix]),
+
+for i in range(np.shape(mps_signal)[0]):
+    hp.write_map(o.dirname+"/maps_sky_signal_Q_"+str(i)+".fits", mps_signal[i,0,:],
+             overwrite=True)
+    hp.write_map(o.dirname+"/maps_sky_signal_U_"+str(i)+".fits", mps_signal[i,1,:],
              overwrite=True)
 
 ###Added
